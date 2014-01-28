df2f2e20eb3738f8f0c6c9dbef66d503f809d83f
Kristjan Metsalu <kristjan.metsalu@ut.ee>
2014-01-28 14:50:11 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snpedia::Application.config.secret_key_base = 'd67c5ebd0d6485e616222a18707d6dfa27956343737458627b41d882d1ebe76e1b0b679d9bf3e97972a7a555f99c3ba3695c79b68b980e0e68c56af32346fcb4'
