19cb74a013a2042246a5dc52c1d2dfef747695fd
Kristjan Metsalu <kristjan.metsalu@ut.ee>
2014-02-07 17:01:32 +0200

    Repairs

--- app/jobs/read_genome_dot_gov_result_job.rb
+++ app/jobs/read_genome_dot_gov_result_job.rb
@@ -20,37 +20,37 @@ class ReadGenomeDotGovResultJob
     end
   end
 
-  ile = open('https://www.genome.gov/admin/gwascatalog.txt').read
+  #ile = open('https://www.genome.gov/admin/gwascatalog.txt').read
 
 #puts file
-
-  titles=[]
-  data=[]
-
-  file.each_line.with_index do | line,index|
-
-    #sline=line.split("\t")
-
-    #puts line
-    sline=line.split("\t")
-
-    if index==0 then
-      sline.each_with_index do |item,index|
-        titles.push index.to_s + ')' + sline[index]
-      end
-    end
-
-    if index==1
-      sline.each_with_index do |item,index|
-        data.push sline[index]
-      end
-
-      break
-    end
-    #break
-  end
-
-  titles.each_with_index do |item,index|
-    puts item + "\t\t\t\t" + data[index]
-  end
+#
+#  titles=[]
+#  data=[]
+#
+#  file.each_line.with_index do | line,index|
+#
+#    #sline=line.split("\t")
+#
+#    #puts line
+#    sline=line.split("\t")
+#
+#    if index==0 then
+#      sline.each_with_index do |item,index|
+#        titles.push index.to_s + ')' + sline[index]
+#      end
+#    end
+#
+#    if index==1
+#      sline.each_with_index do |item,index|
+#        data.push sline[index]
+#      end
+#
+#      break
+#    end
+#    #break
+#  end
+#
+#  titles.each_with_index do |item,index|
+#    puts item + "\t\t\t\t" + data[index]
+#  end
 end
